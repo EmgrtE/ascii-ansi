49db03ae250565902e7d01cfd9d51f597d77a6f1
EmgrtE <emgrte@gmail.com>
2015-02-27 12:16:52 +0200

    add 'ms-dos'

---
+++ ms-dos/ms-dos2.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+cb='\033[38;5;16m'
+cdg='\033[38;5;244m'
+cc='\033[38;5;201m'
+ccd='\033[38;5;90m'
+cdc='\033[38;5;21m'
+cdy='\033[38;5;100m'
+cdr='\033[38;5;88m'
+
+bb='\033[48;5;16m'
+bg='\033[48;5;250m'
+bdg='\033[48;5;244m'
+bdc='\033[48;5;21m'
+bddc='\033[48;5;18m'
+by='\033[48;5;226m'
+bdy='\033[48;5;100m'
+br='\033[48;5;196m'
+bdr='\033[48;5;88m'
+
+
+
+echo -e "  $cb▄▄▄▄       ▄▄▄▄    ▄▄▄▄▄\033[m
+  $bb $bg  $cdg▀$bb \033[m    \033[m $bb $bg  $bdg $bb \033[m $cb▄$bg▀▀$cdg▄$cb▄▄▄$cdg▀$cb▀$bdg▀\033[m$cb▄\033[m
+  $bb $bg $cdg▄ ▀$bb \033[m  \033[m $bb $bg   $bdg $bb \033[m $bb $bg $bdg $bb \033[m   $bb $bg$cb▄$bdg▄$bb \033[m
+  $bb $bg $bdg $cb▄$bg $cdg▀$bb \033[m $bb $bg  $bdg$cb▄$bg $bdg $bb \033[m $cb▀$bg▄$bdg▄$bg$cdg▀$cb▀▀$bdg▀\033[m$cb▄▄\033[m
+  $bb $bg $bdg $bb  $bg $cdg▀$bb $bg  $bb  $bg $bdg $bb \033[m $cb▄▄▄▀▀▀$bg▄ $cdg▀$bdg$cb▀\033[m$cb▄\033[m
+  $bb $bg $bdg $bb \033[m $bb $bg $cdg▀ $bb \033[m $bb $bg $bdg $bb \033[m $bb $bg  $bb \033[m$cb▄▄▄$bg▀ $bdg $bb \033[m
+ $cb▄$bb $bg▄$bdg▄$bb \033[m$cb▄▄$bb $bg▄$bb \033[m $cb▄$bb $bg$cb▄$bdg▄$bb \033[m$cb▄▄▀$bg▄▄▄▄▄▄$bdg▄\033[m$cb▀\033[m
+ $bb $br       $cdr▄$bdr$cb▄$bdc▀$cc▄▀▄▀▄$bddc▀$cb▄$by▀       ▀▀\033[m$cb▄\033[m
+ $bb $br   $cdr▄▄▄▄$bb $bdc$cc▄▀$ccd▄$bb$cc▀$cdc▀$cc▀$bdc$ccd▄$bb $by   $cb▄▄▄▄▄$cdy▄  ▄$bb \033[m
+ $bb $br   $bdr$cb▄▄▄$bb $bdc$cc▀▄$bddc$ccd▀$bb $br  $bb $bddc$cb▄$bb $by  $bb $bddc$ccd▄$bdc$cc▀▄$bb \033[m $cb▀$bdy▄▄▄\033[m$cb▀\033[m
+ $bb $br   $bb \033[m  $bb $bdc$cc▀▄$bddc$ccd▀$bb $br  $bb \033[m $cb▀$cdy$bb▀$by▄$bb $bddc$ccd▄$bdc$cc▀▄$bb $by$cb▀▀\033[m$cb▄▄\033[m
+ $bb $br   $bb \033[m  $bb $bdc$cc▀▄$bddc$ccd▀$bb $br  $bb \033[m   $cb▀$bb $bddc$ccd▄$bdc$cc▀▄$bb $by$cdy▄   $cb▀\033[m$cb▄\033[m
+ $bb $br   $bb \033[m  $cb▀$bb$cc▀$bdc▄$bb $br$cb▀ $cdr▄$bb \033[m $cb▄$by▀▀▀$bddc$cb▄$bdc$cc▀$cb▄\033[m$cb▀▀$bdy▄$by   $bb \033[m
+ $bb $br   $cb▀▀▀▀▀▀  $bdr $bb $cdc▄$cc▄ $bdy $by   $cb▀▀▀▀   $cdy▄$bb \033[m
+ $bb $br         $cdr▄$bb $cdc▄$bdc$cc▄▀▄$cb▀$bdy▄ $by$cdy▄▄     ▄$bdy  $bb \033[m
+ $bb $br$cdr▄$bdr        $cb▄$bb $bddc$ccd▀▄▀▄▀▄$cb▀$bdy▄      ▄▄\033[m$cb▀\033[m
+  $cb▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀ ▀▀▀▀▀▀\033[m"
+
+exit 0
