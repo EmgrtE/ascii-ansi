92878219b893372e21696517dd7298a8f9665f7c
EmgrtE <emgrte@gmail.com>
2015-04-11 20:14:12 +0300

    add the_it_crowd

---
+++ the_it_crowd/the_it_crowd.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#▄▀
+
+o='\033[38;5;172m'
+r='\033[38;5;88m'
+w='\033[38;5;229m'
+
+bo='\033[48;5;172m'
+br='\033[48;5;88m'
+bw='\033[48;5;229m'
+
+
+echo -e "                                                   $r▄▄▄\033[m"
+echo -e "                                                  $br      \033[m$r▄\033[m"
+echo -e "                                                  $br        \033[m$r▄\033[m"
+echo -e "                                                  $br         \033[m$r▄\033[m"
+echo -e "       $o▄▄$bo  \033[m$o▄▄\033[m                               $r▄$br                \033[m"
+echo -e "     $o▄$bo \033[m$o▄$bo      \033[m$o▄▄\033[m                            $br                 \033[m"
+echo -e "    $o▄$bo  \033[m$o▀$bo    \033[m$o▄$bo   \033[m$o▀\033[m             $o▄▄▄▄▄\033[m         $r▀$br    $bo    ▀$br      \033[m"
+echo -e "    $o▀$bo \033[m $bo   \033[m$o▄$bo  \033[m$o▄$bo  \033[m$o▀\033[m           $o▄$bo     $w▄ \033[m$o▄\033[m        $r▀▀$br  $o▀▀▀▀▀▀▀  \033[m$r▀\033[m"
+echo -e "    $o▀$bo  $bw▀ ▀ ▀ ▀$bo  \033[m           $o▄$bo    $w▄$bw  $bo   \033[m          $br $w▀▀▀$bo$r▀$br$w▀▀▀ \033[m"
+echo -e "      $bo $bw $o▄▀ $bo $bw▄ $bo \033[m            $bo   $bw$o▀    ▀$bo  \033[m$o▄\033[m          $bo       \033[m"
+echo -e "      $o▀$bw    $bo $bw  \033[m$o▀\033[m           $bo   $bw $r▀   $r▀ $bo   \033[m          $bo $w▀▀▀▀▀ \033[m"
+echo -e "     $o▄$bo $bw$r▄   $o▀ $r▄\033[m$o▄\033[m           $bo   $bw  $o▄▄▄  $bo   \033[m          $o▀$bo     \033[m$o▀\033[m"
+echo -e "   $o▄$bo   $br $bw$r▄   ▄$br $bo  \033[m          $bo   \033[m$w▀$bw     \033[m$w▀\033[m$bo   \033[m            $bo$w▄▄▄\033[m"
+echo -e " $o▄$bo      $r▀▀▀▀▀    \033[m$o▄\033[m         $o▀▀\033[m  $bo$w▀▀▀\033[m  $o▀▀\033[m         $w▄▄▄▄$bw $br▄$bw \033[m$w▄▄▄▄\033[m"
+echo -e "$bo                  \033[m$o▄\033[m      $br      $bo   $br      \033[m      $bw      $br $bw      \033[m"
